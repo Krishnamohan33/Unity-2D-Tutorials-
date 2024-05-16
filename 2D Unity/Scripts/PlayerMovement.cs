2096a2a27bdee4bddf28d7a5e3d9d9b13667e9fc
Krishnamohan Yagneswaran <110534777+Krishnamohan33@users.noreply.github.com>
2024-05-16 08:00:11 +0530

    Update PlayerMovement.cs

--- 2D Unity/Scripts/PlayerMovement.cs
+++ 2D Unity/Scripts/PlayerMovement.cs
@@ -3,25 +3,10 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
-    private bool isReversed = false; // Flag to track if orientation is reversed
 
     // Update is called once per frame
     void Update()
     {
-        // Check if the character is moving left
-        if (Input.GetAxisRaw("Horizontal") < 0)
-        {
-            isReversed = true;
-            // Reverse the character's orientation to face left
-            transform.eulerAngles = new Vector3(transform.eulerAngles.x, 180f, transform.eulerAngles.z);
-        }
-        else if (Input.GetAxisRaw("Horizontal") > 0) // Check if the character is moving right
-        {
-            isReversed = false;
-            // Reset the character's orientation to face right
-            transform.eulerAngles = new Vector3(transform.eulerAngles.x, 0f, transform.eulerAngles.z);
-        }
-
         // Get input from arrow keys
         float moveHorizontal = Input.GetAxisRaw("Horizontal");
         float moveVertical = Input.GetAxisRaw("Vertical");
